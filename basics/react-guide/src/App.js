a71122177c6bcb4cbe46383171906320607fee0b
davidokun <davidokun99@gmail.com>
2018-07-19 08:36:02 -0500

    Understanding JSX

--- basics/react-guide/src/App.js
+++ basics/react-guide/src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+/*     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">First React App</h1>
         </header>
       </div>
-    );
+    ); */
+
+    /* This is the same as the JSX above. It compiles to html */
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Is this working?"));
   }
 }
 
