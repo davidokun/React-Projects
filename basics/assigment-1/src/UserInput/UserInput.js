9acd4e6edfb040c24515517158c0937377ab7ef5
davidokun <davidokun99@gmail.com>
2018-07-25 09:09:57 -0500

    Adding UserInput and UserOutput components.

---
+++ basics/assigment-1/src/UserInput/UserInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserInput = () => {
+
+    return (
+        <div>
+            <label htmlFor="userInput">User Input: </label>
+            <input id="userInput" type="text"/>
+        </div>
+    )
+};
+
+export default UserInput;
---
+++ basics/assigment-1/src/UserOutput/UserOutput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserOutput = () => {
+
+    return (
+        <div>
+            <p>This is the first paragraph</p>
+            <p>This is the second paragraph</p>
+            <hr/>
+        </div>
+    )
+
+};
+
+export default UserOutput;
