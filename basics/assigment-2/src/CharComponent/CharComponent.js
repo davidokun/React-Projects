f5c0c21258591a5334c1c885110affb4a3586462
davidokun <davidokun99@gmail.com>
2018-08-02 08:32:33 -0500

    Assignment-2 - Adding CharComponent.

---
+++ basics/assigment-2/src/CharComponent/CharComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CharComponent.css';
+
+const CharComponent = (props) => {
+
+    return (
+        <div className="CharComponent">
+            
+        </div>
+    );
+};
+
+export default CharComponent;
