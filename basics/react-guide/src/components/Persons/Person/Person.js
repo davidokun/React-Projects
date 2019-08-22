217e04fae237856def0b501e896a41c91e42ad23
davidokun <davidokun99@gmail.com>
2019-08-22 21:31:22 +0200

    Section-7 - Using React.Fragment.

--- basics/react-guide/src/components/Persons/Person/Person.js
+++ basics/react-guide/src/components/Persons/Person/Person.js
@@ -1,18 +1,19 @@
-import React, {Component} from 'react';
-import Aux from '../../../hoc/Aux';
+import React, {Component, Fragment} from 'react';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
+
+        // Fragment does the same as the Aux component. Implementation is the same
         return (
-            <Aux>
+            <Fragment>
                 <p onClick={this.props.removePerson}
                    key="k1">
                     I'm {this.props.name} and I am {this.props.age} years old
                 </p>
                 <p key="k2">{this.props.children}</p>
                 <input key="3" type="text" onChange={this.props.changed} value={this.props.name}/>
-            </Aux>
+            </Fragment>
         );
     }
 }
